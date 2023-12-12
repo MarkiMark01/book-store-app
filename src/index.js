81e8a2acec2d13d6dc84bc6b84fdf9473a2c6e1a
MarkiMark01 <markishgroup@gmail.com>
2023-12-12 15:58:42 +0200

    Upgrade userroutes page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { store, persistor } from './components/redux/store';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import App from './App';
@@ -10,11 +10,30 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <HashRouter >
         <App />
-      </BrowserRouter>
+      </HashRouter >
     </PersistGate>
   </Provider>
   // </React.StrictMode>
 );
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { Provider } from 'react-redux';
+// import { BrowserRouter } from 'react-router-dom';
+// import { store, persistor } from './components/redux/store';
+// import { PersistGate } from 'redux-persist/lib/integration/react';
+// import App from './App';
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//   <Provider store={store}>
+//     <PersistGate loading={null} persistor={persistor}>
+//       <BrowserRouter>
+//         <App />
+//       </BrowserRouter>
+//     </PersistGate>
+//   </Provider>
+//   // </React.StrictMode>
+// );
 
